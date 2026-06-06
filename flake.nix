4e3d67371910496b70bf2a14615705ce68abdb3c
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2026-06-06 19:06:38 +0530

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Odin programming language environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ odin raylib ];
+        };
+      }
+    );
+}
